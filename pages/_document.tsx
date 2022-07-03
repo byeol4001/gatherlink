3111d3a4ea5a3207be0f82a8b09a041e95d0f300
hanbyeol <byeol4001@gmail.com>
2022-07-03 12:45:19 +0900

    chore: add nest-seo, add default image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,23 @@
 import Document, { Html, Main, NextScript } from "next/document";
+import { NextSeo } from "next-seo";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="ko">
+        <NextSeo
+          openGraph={{
+            // url: `${process.env.CERTIFICATE_S3_ORIGIN}/`,
+            images: [
+              {
+                url: `/public/images/og-gatherlink.png`,
+                width: 600,
+                height: 288,
+                type: "image/png",
+              },
+            ],
+          }}
+        />
         <body>
           <Main />
           <NextScript />
